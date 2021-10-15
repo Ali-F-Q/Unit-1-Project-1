63ebc82f84bc4aa6903e568b82dd6add721b2972
AliFQ <71616390+Ali-F-Q@users.noreply.github.com>
2021-10-15 23:59:22 +0000

    Final

--- Main.java
+++ Main.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 import java.lang.Math;
 
 class Main {
+  //Colours
   public static final String ANSI_RESET = "\u001B[0m";
   public static final String ANSI_RED = "\u001B[31m";
   public static final String ANSI_BLUE = "\u001B[34m";
+  public static final String ANSI_GREEN = "\u001B[32m";
+
   public static void main(String[] args) {
     Scanner rip = new Scanner(System.in);
 
+    //Declarations
     char Choice = 'A';
     String exit;
     int rows;
@@ -25,40 +29,69 @@ class Main {
       }
 
       //Menu
-      System.out.printf("%n%S%n   %s%n   %s%n   %s%n   %s%n   %s%n   %s%n   %s%n","Menu", "A. Addition Table(0-110)", "B. Multiplication Table", "C. Boolean Logic AND Gate", "D. Boolean Logic OR Gate", "E. Modulus Table", "F. Pascal's Triangle", "G. Exit");
+      System.out.printf("%n%S%n   %s%n   %s%n   %s%n   %s%n   %s%n   %s%n   %s%n","Menu", "A. Addition Table", "B. Multiplication Table", "C. Boolean Logic AND Gate", "D. Boolean Logic OR Gate", "E. Modulus Table", "F. Pascal's Triangle", "G. Exit");
       char choice = rip.next().charAt(0);
       Choice = Character.toUpperCase(choice);
 
       switch (Choice){
 
-        // Addition Table
+// Addition Table
         case 'A':
+          System.out.print("\033[H\033[2J");
 
+          //Input for rows and columns
+          System.out.print("How many rows: ");
+          rows = rip.nextInt();
+          System.out.print("How many columns: ");
+          columns = rip.nextInt();
+          table = new int[rows][columns];
+
+          //Box
+          System.out.printf("%n%s%n","Addition Table");
           System.out.print("|");
-          for(int x = 0;x < 65;x++){
+          for(int x = 0;x < (columns+1)*5-1;x++){
+            System.out.print("-");
+          }
+          System.out.printf("%s%n%s","|","|");
+    
+          //Top Row of Table
+          for(int y = 0;y < columns+1;y++){
+            System.out.printf("%-13s%s",ANSI_BLUE + y + ANSI_RESET,"|");
+          }
+          System.out.printf("%n%s","|");
+          for(int x = 0;x < (columns+1)*5-1;x++){
             System.out.print("-");
           }
           System.out.println("|");
 
-          for(int i = 0;i < 101;i += 10){
+          for(int i = 1;i < rows+1;i++){
             System.out.print("|");
 
-            //Claculation
-            for(int y = 0;y < 11;y++){
-              System.out.printf(" %4d%s",i + y,"|");
+            //First Column
+            System.out.printf("%-13s%s",ANSI_BLUE + i + ANSI_RESET,"|");
+            //Calculation
+            for(int y = 1;y < columns+1;y++){
+              table[i-1][y-1] = i + y;
+              if(i==y){
+                System.out.printf("%-13s%s",ANSI_RED + table[i-1][y-1] + ANSI_RESET,"|");
+              }
+              else{
+                System.out.printf("%-4s%s",table[i-1][y-1],"|");
+              }
             }
 
-            System.out.println();
-            System.out.print("|");
-            for(int x = 0;x < 65;x++){
+            //Box
+            System.out.printf("%n%s","|");
+            for(int x = 0;x < (columns+1)*5-1;x++){
               System.out.print("-");
             }
-            System.out.printf("%s%n","|");
+            System.out.println("|");
           }
         break;
 
-        //Multiplication Table
+//Multiplication Table
         case 'B':
+          System.out.print("\033[H\033[2J");
 
           //Input for rows and columns
           System.out.print("How many rows: ");
@@ -105,8 +138,9 @@ class Main {
           }
         break;
 
-        //Boolean Logic AND Gate
+//Boolean Logic AND Gate
         case 'C':
+          System.out.print("\033[H\033[2J");
 
           //Box
           System.out.print("|");
@@ -114,7 +148,7 @@ class Main {
             System.out.print("-");
           }
           System.out.println("|");
-          System.out.printf("%s%14s%14s%14s%n","|", "Condition 1|", "Condition 2|", "AND|");
+          System.out.printf("%s%22s%s%22s%s%22s%s%n","|", ANSI_BLUE + "Condition 1" + ANSI_RESET,"|", ANSI_BLUE + "Condition 2" + ANSI_RESET,"|", ANSI_BLUE + "AND" + ANSI_RESET,"|");
 
           //Creating Table
           for(int i=0;i<4;i++){
@@ -126,7 +160,7 @@ class Main {
             System.out.print("|");
 
             //Calculations
-            System.out.printf("%13s%s%13s%s%13s%s%n",c1[i],"|",c2[i],"|",c1[i]&&c2[i],"|");
+            System.out.printf("%13s%s%13s%s%22s%s%n",c1[i],"|",c2[i],"|", ANSI_RED + (c1[i]&&c2[i]) + ANSI_RESET,"|");
           }
 
           //Box
@@ -137,8 +171,9 @@ class Main {
           System.out.println("|");
         break;
 
-        //Boolean Logic OR Gate
+//Boolean Logic OR Gate
         case 'D':
+          System.out.print("\033[H\033[2J");
 
           //Box
           System.out.print("|");
@@ -146,8 +181,7 @@ class Main {
             System.out.print("-");
           }
           System.out.println("|");
-          System.out.print("|");
-          System.out.printf("%14s%14s%14s%n","Condition 1|", "Condition 2|", "OR|");
+          System.out.printf("%s%22s%s%22s%s%22s%s%n","|", ANSI_BLUE + "Condition 1" + ANSI_RESET,"|", ANSI_BLUE + "Condition 2" + ANSI_RESET,"|", ANSI_BLUE + "OR" + ANSI_RESET,"|");
 
           //Creating Table
           for(int i=0;i<4;i++){
@@ -159,7 +193,7 @@ class Main {
             System.out.print("|");
 
             //Calculations
-            System.out.printf("%13s%s%13s%s%13s%s%n",c1[i],"|",c2[i],"|",c1[i]||c2[i],"|");
+            System.out.printf("%13s%s%13s%s%22s%s%n",c1[i],"|",c2[i],"|", ANSI_RED + (c1[i]||c2[i]) + ANSI_RESET,"|");
           }
 
           //Box
@@ -170,27 +204,66 @@ class Main {
           System.out.println("|");
         break;
 
-        //Modulus Table---------------------------------------------------------
+//Modulus Table
         case 'E':
+          System.out.print("\033[H\033[2J");
 
-        System.out.print("  ");
-          for(int x = 1;x < 11;x++){
-            System.out.printf("%4d",x);
+          //Input for rows and columns
+          System.out.print("How many rows: ");
+          rows = rip.nextInt();
+          System.out.print("How many columns: ");
+          columns = rip.nextInt();
+          table = new int[rows][columns];
+
+          //Box
+          System.out.printf("%n%s%n","Modulus Table(Remainder After Division)");
+          System.out.print("|");
+          for(int x = 0;x < (columns+1)*5-1;x++){
+            System.out.print("-");
           }
-          System.out.println();
-          for(int i = 1;i < 11;i++){
-            System.out.printf("%2d",i);
-            for(int y = 1;y < 11;y++){
-              int num = y%i;
-              System.out.printf("%4d",num);
+          System.out.printf("%s%n%s","|","|");
+    
+          //Top Row of Table
+          for(int y = 0;y < columns+1;y++){
+            System.out.printf("%-13s%s",ANSI_BLUE + y + ANSI_RESET,"|");
+          }
+          System.out.printf("%n%s","|");
+          for(int x = 0;x < (columns+1)*5-1;x++){
+            System.out.print("-");
+          }
+          System.out.println("|");
+
+          for(int i = 1;i < rows+1;i++){
+            System.out.print("|");
+
+            //First Column
+            System.out.printf("%-13s%s",ANSI_BLUE + i + ANSI_RESET,"|");
+            //Calculation
+            for(int y = 1;y < columns+1;y++){
+              table[i-1][y-1] = y % i;
+              if(y==i||i==1){
+                System.out.printf("%-13s%s", ANSI_RED + table[i-1][y-1] + ANSI_RESET,"|");
+              }
+              else if(table[i-1][y-1] == y){
+                System.out.printf("%-13s%s", ANSI_GREEN + table[i-1][y-1] + ANSI_RESET,"|");
+              }
+              else{
+                System.out.printf("%-4s%s",table[i-1][y-1],"|");
+              }
             }
-            System.out.println();
+
+            //Box
+            System.out.printf("%n%s","|");
+            for(int x = 0;x < (columns+1)*5-1;x++){
+              System.out.print("-");
+            }
+            System.out.println("|");
           }
         break;
-        //Modulus Table---------------------------------------------------------
 
-        //Pascal's Triangle
+//Pascal's Triangle
         case 'F':
+          System.out.print("\033[H\033[2J");
 
           //Question
           System.out.print("Number of rows: ");
